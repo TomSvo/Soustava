799bb420a668c50be789580d5357c332f7be09b9
TomSvo <svobodat0@gmail.com>
2017-05-06 22:46:31 +0200

    commit

---
+++ SoustavaFormApp/SoustavaFormApp/FyzLaws.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoustavaFormApp
+{
+    public static class FyzLaws
+    {
+        public static decimal Pritazlivost(decimal g, decimal a)
+        {
+            return  g* a;
+        }
+            
+       
+    }
+}
